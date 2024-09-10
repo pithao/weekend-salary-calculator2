70d61ce288b0351fe44f05e89efebc7ec643ce78
Pa Thao <paiathao@gmail.com>
2024-09-09 21:01:47 -0500

    cant get submit button to work

--- client.js
+++ client.js
@@ -1,15 +1,41 @@
+console.log('hello world');
+
 // * When the "Submit" button is clicked:
 //     * A new employee row should be added to the table.
 //     * The footer's total monthly cost should be updated.
 //     * The form inputs should be cleared out.
 
 
- // Find the input field and assign the value to our variable
- let firstName = document.querySelector('#firstName-input').value;
- console.log(firstName);
- //! .value always returns a string, for a number use Number(string)
- let lastName = document.querySelector('#lastName-input').value;
- let idNumber = document.querySelector('#idNumber-input').value;
- let annualSalary = document.querySelector('#annualSalary-input').value;
+function submitForm(event) {
+    console.log('submitForm');
+    // Stop the page from refreshing
+    event.preventDefault();
 
- 
+    let firstNameVal = document.querySelector('#firstName-input').value;
+    console.log(firstNameVal);
+    let lastNameVal = document.querySelector('#lastName-input').value;
+    let idNumberVal = document.querySelector('#idNumber-input').value;
+    let annualSalaryVal = document.querySelector('#annualSalary-input').value;
+
+    let employeeTable = document.querySelector('#employeeData');
+    employeeTable.innerHTML += `
+        <tr>
+            <td>${currentIndex}</td>
+            <td>${firstNameVal}</td>
+            <td>${lastNameVal}</td>
+            <td>${idNumberValVal}</td>
+            <td>${annualSalaryVal}</td>
+            <td><button onClick = "handleClickDelete(event)" id= "currentIndex">Delete</button></td>
+        </tr>
+    `;
+    currentIndex += 1;
+}
+
+    function handleClickDelete(event) {
+        console.log("Delete button clicked!");
+       
+       let row = event.target.parentElement;
+
+       let table = document.getElementById("employeeData");
+        row.remove();
+        }
